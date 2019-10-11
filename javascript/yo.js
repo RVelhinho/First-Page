9a12636d952497054e018c8d7a0c668923050f12
Ricardo Velhinho <36649075+RVelhinho@users.noreply.github.com>
2019-10-11 21:58:08 +0100

    mobile here!!

--- javascript/yo.js
+++ javascript/yo.js
@@ -1,36 +1,42 @@
 
 function main(){
     window.onbeforeunload = function () {
-        window.scrollTo(0, 0);
+        document.getElementById("1").scrollIntoView({ block: 'end',  behavior: 'smooth' });
     }
-    
+    var _current = 1;
     window.addEventListener('wheel',function(e) {
-        if(e.deltaY < 0 && scrollY > 800 && scrollY < 850){
+        const li1 = document.getElementById('li1');
+        const li2 = document.getElementById('li2');
+        const li3 = document.getElementById('li3');
+        if(e.deltaY < 0 && _current==2){
             document.getElementById("1").scrollIntoView({ block: 'end',  behavior: 'smooth' });
-            document.getElementById('li1').style.fontSize = '2.5vw';
-            document.getElementById('li1').style.opacity = '1';
-            document.getElementById('li2').style.fontSize = '1.3vw';
-            document.getElementById('li2').style.opacity = '.5';
-            document.getElementById('li3').style.fontSize = '1.3vw';
-            document.getElementById('li3').style.opacity = '.5';
+            li1.style.fontSize = '2.5vw';
+            li1.style.opacity = '1';
+            li2.style.fontSize = '1.3vw';
+            li2.style.opacity = '.5';
+            li3.style.fontSize = '1.3vw';
+            li3.style.opacity = '.5';
+            _current = 1;
          }
-        else if((e.deltaY < 0 && scrollY > 1600 && scrollY < 1650) || (e.deltaY > 0 && scrollY >= 0 && scrollY < 50)) {
+        else if((e.deltaY < 0 && _current==3) || (e.deltaY > 0 && _current==1)){
             document.getElementById("2").scrollIntoView({ block: 'end',  behavior: 'smooth' });
-            document.getElementById('li1').style.fontSize = '1.3vw';
-            document.getElementById('li1').style.opacity = '.5';
-            document.getElementById('li2').style.fontSize = '2.5vw';
-            document.getElementById('li2').style.opacity = '1';
-            document.getElementById('li3').style.fontSize = '1.3vw';
-            document.getElementById('li3').style.opacity = '.5';
+            li1.style.fontSize = '1.3vw';
+            li1.style.opacity = '.5';
+            li2.style.fontSize = '2.5vw';
+            li2.style.opacity = '1';
+            li3.style.fontSize = '1.3vw';
+            li3.style.opacity = '.5';
+            _current = 2;
         }
-        else if(e.deltaY > 0 && scrollY > 800 && scrollY < 850){
+        else if(e.deltaY > 0 && _current==2){
             document.getElementById("3").scrollIntoView({ block: 'end',  behavior: 'smooth' });
-            document.getElementById('li1').style.fontSize = '1.3vw';
-            document.getElementById('li1').style.opacity = '.5';
-            document.getElementById('li2').style.fontSize = '1.3vw';
-            document.getElementById('li2').style.opacity = '.5';
-            document.getElementById('li3').style.fontSize = '2.5vw';
-            document.getElementById('li3').style.opacity = '1';
+            li1.style.fontSize = '1.3vw';
+            li1.style.opacity = '.5';
+            li2.style.fontSize = '1.3vw';
+            li2.style.opacity = '.5';
+            li3.style.fontSize = '2.5vw';
+            li3.style.opacity = '1';
+            _current = 3;
         }
         
     });
